afdb55d029ce785874664140f3c179d9b9af57fd
abdelghany100 <127979526+abdelghany100@users.noreply.github.com>
2023-10-22 20:41:00 +0300

    Update Modal.jsx

--- src/component/UI/Modal.jsx
+++ src/component/UI/Modal.jsx
@@ -31,7 +31,7 @@ const Modal = ({ activeID, setShowModal }) => {
                     <button className="bg-primaryColor text-white py-2 px-4 my-8 rounded-[8px] font-[500] hover:bg-headingColor ease-in duration-300" >Live Site</button>
              </a>
         </div>
-        <button onClick={()=>setShowModal(false)} className="w-[1.8rem]  h-[1.8rem] bg-[white] absolute top-[1.7rem]  right-[1.7rem] text-[25px] flex items-center  justify-center rounded-[3px] leading-0 cursor-pointer">&times;</button>
+        <button onClick={()=>setShowModal(false)} className="w-[1.8rem]  h-[1.8rem] bg-primaryColor absolute top-[40rem]  right-[1.7rem] text-[25px] flex items-center  justify-center rounded-[3px] leading-0 cursor-pointer">&times;</button>
 
       </div>
     </div>
